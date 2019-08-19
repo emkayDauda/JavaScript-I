6267a6a46b5a817e23cb34e10d81a1026a146fda
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-19 13:27:15 +0100

    Give Kennan ability to speak

--- assignments/objects.js
+++ assignments/objects.js
@@ -31,6 +31,7 @@ const Kennan = {
   email: "kdiben1@tinypic.com",
   "first name": "Kennan",
   gender: "M",
+  speak: () => "I can speak, Muhahahaha!"
 }
 
 const Kevin = {
@@ -69,7 +70,7 @@ console.log(Gannie["first name"])
 console.log(Antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+console.log(Kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
